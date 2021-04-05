588d3253e49cf5ead1c636535cff6c23d952905c
advy99 <advy99@gmail.com>
2021-04-05 21:36:22 +0200

    clase colorRGB

--- proyecto/color_rgb.lua
+++ proyecto/color_rgb.lua
@@ -9,10 +9,16 @@ end
 
 function ColorRGB:new (r, g, b)
 	obj = {}
-	obj.r = r or 255
-	obj.g = g or 255
-	obj.b = b or 255
+	obj.r = r or 1
+	obj.g = g or 1
+	obj.b = b or 1
 	setmetatable(obj, self)
 	self.__index = self
 	return obj
+end
+
+function ColorRGB:changeColor(r, g, b)
+	self.r = r
+	self.g = g
+	self.b = b
 end

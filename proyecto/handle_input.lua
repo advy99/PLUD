510a6aa64993eaae8fb29afa15b08210b17c87ba
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-04-30 18:33:23 +0200

    teclas de ataque para los jugadores

--- proyecto/handle_input.lua
+++ proyecto/handle_input.lua
@@ -1,5 +1,7 @@
 
 function handleKeyboard(dt)
+
+
 	if love.keyboard.isDown("right") then --press the right arrow key to push the ball to the right
 		objects.player1:move(1)
 	end
@@ -11,7 +13,6 @@ function handleKeyboard(dt)
 	if love.keyboard.isDown("up") then --press the up arrow key to set the ball in the air
 		objects.player1:jump()
 	end
-
 
 
 	if love.keyboard.isDown("d") then --press the right arrow key to push the ball to the right
@@ -34,4 +35,14 @@ function love.keypressed(k)
    if k == 'escape' then
       love.event.quit()
    end
+
+	if k == "e" then
+		objects.player2:attack()
+	end
+
+	if k == "enter" then
+		objects.player1:attack()
+	end
+
+
 end

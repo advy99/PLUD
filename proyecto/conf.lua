33870619898ea92544fc1f557039907b5d1d52c0
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-05-23 20:26:16 +0200

    en realidad uso el width y heght del config, no de este

--- proyecto/conf.lua
+++ proyecto/conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.title = "Nombre provisional - 2021"                  -- The title of the window the game is in (string)
     t.version = "11.3"                    -- The LÖVE version this game was made for (string)
-    t.window.width = 1280                 -- The window width (number)
-    t.window.height = 768                 -- The window height (number)
+    -- t.window.width = 1280                 -- The window width (number)
+    -- t.window.height = 768                 -- The window height (number)
 end

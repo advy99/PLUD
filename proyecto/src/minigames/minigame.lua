cc8e3580ae7b36fc642d947b8c2137b3c2c23d54
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-05-23 20:28:05 +0200

    clase base de minijuego, que tendrá el nivel y ya

---
+++ proyecto/src/minigames/minigame.lua
@@ -0,0 +1,55 @@
+local class = require "lib/middleclass"
+
+
+MiniGame = class("MiniGame")
+
+function MiniGame:initialize(minigame_level)
+
+	self.level = Level(minigame_level)
+
+end
+
+
+function MiniGame:update(dt)
+
+	self.level:update(dt)
+
+end
+
+
+function MiniGame:draw()
+	self.level:draw()
+
+end
+
+function MiniGame:loadLevel(level_name)
+	self.level = Level(level_name)
+end
+
+
+function MiniGame:handleEvent(object, event)
+
+	-- no debería pasar nunca, ya que el objeto está en el nivel, pero por si acaso
+	if self.level.objects[object] ~= nil then
+
+		if event == Events.PLAYER_LAND_PLATFORM then
+			self.level.objects[object]:setMode("grounded")
+
+		elseif event == Events.PLAYER_LEAVE_PLATFORM then
+
+			if self.level.objects[object]:getMode() ~= "jumping" then
+				self.level.objects[object]:setMode("falling")
+			end
+
+		elseif event == Events.EXIT_GAME then
+			love.event.quit()
+		end
+	end
+
+end
+
+
+
+function MiniGame:handleEventBetweenObjects(object_a, object_b, event)
+
+end

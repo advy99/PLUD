adc1b81e939b8d6cc57ea89306110874cd1f17cc
advy99 <advy99@gmail.com>
2021-05-29 20:20:02 +0200

    empezamos clase configuración

---
+++ proyecto/src/configuration.lua
@@ -0,0 +1,49 @@
+local class = require "lib/middleclass"
+local LIP = require "lib/LIP"
+
+Configuration = class("Configuration")
+
+function Configuration:initialize()
+	self.config = nil
+	self:loadConfiguration()
+end
+
+
+function Configuration:setVSYNC(mode)
+	assert(type(mode) == "boolean", "setVSYNC expects a boolean")
+
+	self.config.screen.vsync = mode
+end
+
+function Configuration:getVSYNC(mode)
+	return self.config.screen.vsync
+end
+
+function Configuration:setShowFPS(mode)
+	assert(type(mode) == "boolean", "setShowFPS expects a boolean")
+
+	self.config.screen.showFPS = mode
+
+end
+
+function Configuration:getShowFPS()
+	return self.config.screen.showFPS
+end
+
+function Configuration:setFullscreen(mode)
+	assert(type(mode) == "boolean", "setFullscreen expects a boolean")
+
+	self.config.screen.fullscreen = mode
+end
+
+function Configuration:getFullscreen()
+	return self.config.screen.fullscreen
+end
+
+function Configuration:saveConfig()
+	LIP.save("config/config.ini", self.config)
+end
+
+function Configuration:loadConfiguration()
+	self.config = LIP.load("config/config.ini")
+end

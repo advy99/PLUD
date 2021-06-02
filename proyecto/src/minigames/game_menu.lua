1d4a2be29b57d7fd04068ec62be3d16aefbe37b7
advy99 <advy99@gmail.com>
2021-06-02 16:08:25 +0200

    limpio el codigo variables sin usar

--- proyecto/src/minigames/game_menu.lua
+++ proyecto/src/minigames/game_menu.lua
@@ -24,13 +24,6 @@ function GameMenu:draw()
 	love.graphics.setColor(1, 1, 1)
 	MiniGame.draw(self)
 
-	local width = love.graphics.getWidth()
-	local height = love.graphics.getHeight()
-
- 	mainFont = love.graphics.newFont("fonts/kirbyss.ttf", 50)
-	love.graphics.setFont(mainFont)
-	-- love.graphics.printf("JUGAR", -width * 0.165 ,  height * 0.73, 1000, "center")
-	-- love.graphics.printf("SALIR", width * 0.385,  height * 0.73, 1000, "center")
 
 	love.graphics.draw(self.game_name_image, 480, 70, 0, 0.1, 0.1)
 

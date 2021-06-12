bf8bab936fe9fc81f6778968449a4e9a17ed2e33
advy99 <advy99@gmail.com>
2021-06-12 20:33:45 +0200

    preparo para animar el titulo

--- proyecto/src/minigames/menus/title_menu.lua
+++ proyecto/src/minigames/menus/title_menu.lua
@@ -8,7 +8,10 @@ function TitleMenu:initialize(num_players)
 
 	Menu.initialize(self, "level_title", num_players)
 
-	self.game_name_image = love.graphics.newImage("img/lose_to_win.png")
+	local game_name_image = love.graphics.newImage("img/lose_to_win_animated.png")
+	self.sprite_width = 3000
+	self.sprite_height = 2600
+	self.title_animation = newAnimation(game_name_image, 0, self.sprite_width, self.sprite_height, 1)
 
 
 end
@@ -18,6 +21,9 @@ function TitleMenu:update(dt)
 
 	Menu.update(self, dt)
 
+	self.title_animation.currentTime = (self.title_animation.currentTime + dt) % self.title_animation.duration
+
+
 end
 
 
@@ -25,8 +31,9 @@ function TitleMenu:draw()
 	love.graphics.reset()
 	Menu.draw(self)
 
+	local quad = math.floor(self.title_animation.currentTime / self.title_animation.duration * #self.title_animation.quads) + 1
 
-	love.graphics.draw(self.game_name_image, 480, 70, 0, 0.1, 0.1)
+	love.graphics.draw(self.title_animation.spriteSheet, self.title_animation.quads[quad], 480, 70, 0, 0.1, 0.1 )
 
 end
 

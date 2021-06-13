7f0c0d852ee3c3075934d97d7b4210366844a60e
advy99 <advy99@gmail.com>
2021-06-13 20:18:41 +0200

    idiomas

---
+++ proyecto/languages/english.lua
@@ -0,0 +1,22 @@
+English = {}
+
+
+English.PLAY_GAME = "GAME STARTS\nIN... "
+English.ENTER_PRACTICE = "PRACTICE MODE\n IN..."
+English.ENTER_OPTIONS = "SETTINGS\nIN... "
+English.EXIT_GAME = "EXIT GAME\nIN..."
+
+English.EXIT_SAVING_OPTIONS = "QUIT AND SAVE\nIN..."
+English.QUIT_TO_MENU = "QUIT TO MENU IN .."
+
+English.CONFIGURATION = "CONFIGURATION"
+English.SCREEN = "SCREEN"
+English.VOLUME = "VOLUME"
+English.MUSIC = "MUSIC"
+English.SFX = "SFX"
+English.MUTE = "MUTE ALL"
+English.CONTROLS = "CONTROLS"
+English.JUMP = "JUMP"
+English.LEFT = "LEFT"
+English.RIGHT = "RIGHT"
+English.BACK = "BACK"

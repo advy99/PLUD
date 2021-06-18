489ced5398af5bd43c8feb83243ebc13fcbc2a53
advy99 <advy99@gmail.com>
2021-06-18 12:00:52 +0200

    arreglo llamadas a new y las acciones recaen sobre el minijuego al principio de la lista

--- proyecto/src/minigames/play_state.lua
+++ proyecto/src/minigames/play_state.lua
@@ -15,9 +15,9 @@ function PlayState:initialize(num_players)
 	self.minigames = {}
 	-- insertamos los juegos de forma aleatoria
 	local pos = math.random(0, 2)
-	table.insert(self.minigames, (pos % 3) + 1, BombTag:BombTag:new(num_players))
-	table.insert(self.minigames, ((pos + 1) % 3) + 1, DeathBall:BombTag:new(num_players))
-	table.insert(self.minigames, ((pos + 2) % 3) + 1, VirusFall:BombTag:new(num_players))
+	table.insert(self.minigames, (pos % 3) + 1, BombTag:new(num_players))
+	table.insert(self.minigames, ((pos + 1) % 3) + 1, DeathBall:new(num_players))
+	table.insert(self.minigames, ((pos + 2) % 3) + 1, VirusFall:new(num_players))
 
 	self.scores = {}
 
@@ -25,22 +25,19 @@ end
 
 
 function PlayState:update(dt)
-
-
+	self.minigames[0]:update(dt)
 end
 
 
 function PlayState:draw()
-
+	self.minigames[0]:draw()
 end
 
 function PlayState:handleEvent(object, event)
-
-
+	self.minigames[0]:handleEvent(object, event)
 end
 
 
 function PlayState:handleEventBetweenObjects(object_a, object_b, event)
-
-
+	self.minigames[0]:handleEventBetweenObjects(object_a, object_b, event)
 end

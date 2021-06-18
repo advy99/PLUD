249a44267d938082608f5f78c79991a8d5d2a8cd
Gsandoval96 <gsandovalschmidt@gmail.com>
2021-06-18 17:17:01 +0200

    Añadido menu para los resultados

---
+++ proyecto/src/minigames/menus/result_menu.lua
@@ -0,0 +1,29 @@
+require("src/Menus/menus/menu")
+
+local class = require "lib/middleclass"
+
+ResultMenu = Menu:subclass('ResultMenu')
+
+function ResultMenu:initialize(num_players)
+	Menu.initialize(self, "level_result", num_players)
+end
+
+
+function ResultMenu:update(dt)
+	Menu.update(self, dt)
+end
+
+
+function ResultMenu:draw()
+	love.graphics.reset()
+	Menu.draw(self)
+end
+
+function ResultMenu:handleEvent(object, event)
+	Menu.handleEvent(self, object, event)
+end
+
+
+function ResultMenu:handleEventBetweenObjects(object_a, object_b, event)
+	Menu.handleEventBetweenObjects(self, object_a, object_b, event)
+end

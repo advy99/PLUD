ea57e96b8404a90e942f0bf6e04d3ba03bc83694
advy99 <advy99@gmail.com>
2021-06-18 16:16:49 +0200

    animacion de cortina acabada

--- proyecto/src/minigames/play_state.lua
+++ proyecto/src/minigames/play_state.lua
@@ -25,43 +25,89 @@ function PlayState:initialize(num_players)
 
 	self:saveScores()
 
-	self.TIME_PER_MINIGAME = 10
+	self.curtain = love.graphics.newImage("img/curtain.png")
+
+	self.left_curtain_animation_position = -Constants.DEFAULT_WIDTH / 2
+	self.right_curtain_animation_position = Constants.DEFAULT_WIDTH
+
+	self.curtain_animation_time = 0.75
+
+	self.TIME_PER_MINIGAME = 3
+	self.TIME_CLOSE_CURTAINS = self.TIME_PER_MINIGAME + self.curtain_animation_time
+	self.TIME_OPEN_CURTAINS = self.TIME_CLOSE_CURTAINS + self.curtain_animation_time
 	self.elapsed_time = 0
+
+	self.has_changed = false
+	self.show_results = false
+
 
 end
 
 
 function PlayState:update(dt)
-	self.minigames[#self.minigames]:update(dt)
 
-	self.elapsed_time = self.elapsed_time + dt
-
-	if self.elapsed_time >= self.TIME_PER_MINIGAME then
-		self.elapsed_time = 0
-		if #self.minigames > 0 then
-			self:saveScores()
-			table.remove(self.minigames, #self.minigames)
-			self.level = self.minigames[#self.minigames].level
-			self:setScores()
-		else
-			-- TODO acabar estado de juego, ha terminado la partida
-		end
+	if not self.show_results then
+		self.elapsed_time = self.elapsed_time + dt
 	end
 
+	-- si nos pasamos del tempo de juego
+	if self.elapsed_time >= self.TIME_PER_MINIGAME and not self.show_results then
+
+		-- si no han acabado de cerrarse las cortinas
+		if self.elapsed_time <= self.TIME_CLOSE_CURTAINS then
+			if self.left_curtain_animation_position < 0 then
+				self.left_curtain_animation_position = self.left_curtain_animation_position + 768 * dt / self.curtain_animation_time
+				self.right_curtain_animation_position = self.right_curtain_animation_position - 768 * dt / self.curtain_animation_time
+			end
+		elseif self.elapsed_time <= self.TIME_OPEN_CURTAINS then
+			-- si no se ha cambiado de minijuego
+			if #self.minigames > 0 and not self.has_changed then
+				self:saveScores()
+				table.remove(self.minigames, #self.minigames)
+				self.level = self.minigames[#self.minigames].level
+				self:setScores()
+				self.has_changed = true
+			end
+
+			if self.left_curtain_animation_position > -Constants.DEFAULT_WIDTH / 2 then
+				self.left_curtain_animation_position = self.left_curtain_animation_position - 768 * dt / self.curtain_animation_time
+				self.right_curtain_animation_position = self.right_curtain_animation_position + 768 * dt / self.curtain_animation_time
+			end
+		else
+			self.elapsed_time = 0
+			self.has_changed = false
+			if #self.minigames == 0 then
+				self.show_results = true
+			end
+		end
+	else
+		self.minigames[#self.minigames]:update(dt)
+	end
 end
 
 
 function PlayState:draw()
 	self.minigames[#self.minigames]:draw()
+
+	if self.elapsed_time >= self.TIME_PER_MINIGAME then
+		love.graphics.draw(self.curtain, self.left_curtain_animation_position, 0)
+		love.graphics.draw(self.curtain, self.right_curtain_animation_position, 0)
+	end
+
 end
 
 function PlayState:handleEvent(object, event)
-	self.minigames[#self.minigames]:handleEvent(object, event)
+	if self.elapsed_time < self.TIME_PER_MINIGAME then
+		self.minigames[#self.minigames]:handleEvent(object, event)
+	end
 end
 
 
 function PlayState:handleEventBetweenObjects(object_a, object_b, event)
-	self.minigames[#self.minigames]:handleEventBetweenObjects(object_a, object_b, event)
+	if self.elapsed_time < self.TIME_PER_MINIGAME then
+		self.minigames[#self.minigames]:handleEventBetweenObjects(object_a, object_b, event)
+	end
+
 end
 
 

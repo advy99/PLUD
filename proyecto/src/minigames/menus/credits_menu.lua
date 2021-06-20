5e1009bb0980d3c056da1e2e680e7a28be951a2a
Gsandoval96 <gsandovalschmidt@gmail.com>
2021-06-20 16:45:39 +0200

    Rediseñados los cŕeditos del juego

--- proyecto/src/minigames/menus/credits_menu.lua
+++ proyecto/src/minigames/menus/credits_menu.lua
@@ -16,20 +16,34 @@ function CreditsMenu:initialize(num_players)
 	self.config_background = TextBox:new("", self.background_box[1], self.background_box[2], self.background_box[3], self.background_box[4], 40, 0.9, text_color, box_color)
 
 	local up_box = {self.background_box[1] + self.background_box[3]/2 - 200, 170, 400, 60}
-	local down_box = {self.background_box[1] + self.background_box[3]/2 - 200, 350, 400, 60}
+	local down_box_page_1 = {self.background_box[1] + self.background_box[3]/2 - 200, 350, 400, 60}
 
 	self.made_by = TextBox:new(language.MADE_BY, up_box[1], up_box[2], up_box[3], up_box[4], 35, 0.9, text_color, box_color)
 	self.author_1 = TextBox:new("Guillermo Sandoval", self.background_box[1] , 250, self.background_box[3], 30, 35, 0, text_color, box_color)
 	self.author_2 = TextBox:new("Antonio D. Villegas", self.background_box[1], 300, self.background_box[3], 30, 35, 0, text_color, box_color)
 
-	self.special_thanks = TextBox:new(language.THANKS, down_box[1], down_box[2], down_box[3], down_box[4], 35, 0.9, text_color, box_color)
-	self.thanks = {}
+	self.art = TextBox:new(language.ART, down_box_page_1[1], down_box_page_1[2], down_box_page_1[3], down_box_page_1[4], 35, 0.9, text_color, box_color)
+	self.artist = {}
 
-	local names = {"Calciumtrice","PixelFrog","Khrinx","Monplaisir","Brian Kent"}
+	local names_art = {"Calciumtrice","PixelFrog","Khrinx","dbAF23"}
 
-	for i=1, 5, 1 do
-		self.thanks[i] = TextBox:new(names[i], self.background_box[1] , 430 + 50*(i-1), self.background_box[3], 30, 35, 0, text_color, box_color)
+	for i=1, #names_art, 1 do
+		self.artist[i] = TextBox:new(names_art[i], self.background_box[1] , 430 + 50*(i-1), self.background_box[3], 30, 35, 0, text_color, box_color)
 	end
+
+	self.sound = TextBox:new(language.SOUND, up_box[1], up_box[2], up_box[3], up_box[4], 35, 0.9, text_color, box_color)
+	self.musician = {}
+
+	local names_music = {"Monsplaisir", "Zuzek06", "Lukeo135", "Independent.nu", "SomeSine"}
+
+	for i=1, #names_music, 1 do
+		self.musician[i] = TextBox:new(names_music[i], self.background_box[1] , 250 + 50*(i-1), self.background_box[3], 30, 35, 0, text_color, box_color)
+	end
+
+	local down_box_page_2 = {self.background_box[1] + self.background_box[3]/2 - 200, 500, 400, 60}
+
+	self.font = TextBox:new(language.FONT, down_box_page_2[1], down_box_page_2[2], down_box_page_2[3], down_box_page_2[4], 35, 0.9, text_color, box_color)
+	self.author_font = TextBox:new("Brian Kent", self.background_box[1] , 580, self.background_box[3], 30, 35, 0, text_color, box_color)
 
 	self.active_page = 1
 	self.NUM_PAGES = 2
@@ -71,18 +85,21 @@ function CreditsMenu:draw()
 		self.author_1:draw()
 		self.author_2:draw()
 
-	elseif self.active_page == 2 then
-		self.special_thanks:draw()
-		for i=1, 5, 1 do
-			self.thanks[i]:draw()
+		self.art:draw()
+		for _, object in pairs(self.artist) do
+			object:draw()
 		end
 
+	elseif self.active_page == 2 then
+		self.sound:draw()
+		for _, object in pairs(self.musician) do
+			object:draw()
+		end
+
+		self.font:draw()
+		self.author_font:draw()
+
 	end
-
-
-
-
-
 
 	local buttons_position = {self.background_box[1] + self.background_box[3]/2 , 640}
 	local text_color = {1, 1, 1}
